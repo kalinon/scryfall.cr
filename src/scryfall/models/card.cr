fd9c6ed494ce6fd2520071cda6d7f3e9d5611f99
Holden Omans <holden.omans@gmail.com>
2021-09-12 09:37:00 -0400

    removed foil and non-foil. added new finishes attribute

--- src/scryfall/models/card.cr
+++ src/scryfall/models/card.cr
@@ -33,7 +33,6 @@ module Scryfall
     getter color_indicator : Array(String) = Array(String).new
     getter colors : Array(String) = Array(String).new
     getter edhrec_rank : Int32? = nil
-    getter foil : Bool
     getter hand_modifier : String? = nil
     getter keywords : Array(String) = Array(String).new
     getter layout : String
@@ -42,52 +41,54 @@ module Scryfall
     getter loyalty : String? = nil
     getter mana_cost : String? = nil
     getter name : String
-    getter nonfoil : Bool
+    # getter? foil : Bool
+    # getter? nonfoil : Bool
+    getter finishes: Array(String) = Array(String).new
     getter oracle_text : String? = nil
-    getter oversized : Bool
+    getter? oversized : Bool
     getter power : String? = nil
     getter produced_mana : Array(String) = Array(String).new
     getter type_line : String
-    getter reserved : Bool
+    getter? reserved : Bool
     getter toughness : String? = nil
     getter type_line : String
 
     # Print Fields
     getter artist : String? = nil
-    getter booster : Bool
+    getter? booster : Bool
     getter border_color : String
     getter card_back_id : UUID
     getter collector_number : String
-    getter content_warning : Bool = false
-    getter digital : Bool
+    getter? content_warning : Bool = false
+    getter? digital : Bool
     getter flavor_name : String? = nil
     getter flavor_text : String? = nil
     getter frame_effects : Array(String) = Array(String).new
     getter frame : String
-    getter full_art : Bool
+    getter? full_art : Bool
     getter games : Array(String) = Array(String).new
-    getter highres_image : Bool
+    getter? highres_image : Bool
     getter illustration_id : URI? = nil
     getter image_uris : Hash(String, URI) = Hash(String, URI).new
     getter prices : Hash(String, String?)
     getter printed_name : String? = nil
     getter printed_text : String? = nil
     getter printed_type_line : String? = nil
-    getter promo : Bool
+    getter? promo : Bool
     getter promo_types : Array(String) = Array(String).new
     getter purchase_uris : Hash(String, String)
     getter rarity : String
     getter related_uris : Hash(String, String)
     getter released_at : String
-    getter reprint : Bool
+    getter? reprint : Bool
     getter set_search_uri : URI
     getter set_name : String
     getter set_type : String
     getter set_uri : URI
     getter set : String
-    getter story_spotlight : Bool
-    getter textless : Bool
-    getter variation : Bool
+    getter? story_spotlight : Bool
+    getter? textless : Bool
+    getter? variation : Bool
     getter variation_of : UUID? = nil
     getter watermark : String? = nil
     getter preview : Hash(String, String) = Hash(String, String).new
