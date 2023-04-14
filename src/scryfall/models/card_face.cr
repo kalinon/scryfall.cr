1fb42eb7c015531e09e77279cb6165ddfb0543ee
Holden Omans <holden.omans@gmail.com>
2023-04-14 17:51:51 -0400

    add battle defense to card_face

--- src/scryfall/models/card_face.cr
+++ src/scryfall/models/card_face.cr
@@ -13,18 +13,20 @@ module Scryfall
     getter flavor_text : String? = nil
     getter illustration_id : UUID? = nil
     getter image_uris : Hash(String, URI)? = nil
-    getter loyalty : String? = nil
     getter mana_cost : String
     getter cmc : Float32? = nil
     getter name : String
     getter object : String
     getter oracle_text : String? = nil
-    getter power : String? = nil
     getter printed_name : String? = nil
     getter printed_text : String? = nil
     getter printed_type_line : String = ""
-    getter toughness : String? = nil
     getter type_line : String = ""
     getter watermark : String = ""
+
+    getter power : String? = nil
+    getter toughness : String? = nil
+    getter loyalty : String? = nil
+    getter defense : String? = nil
   end
 end
