7f73c55a843dbae050230ec3b8c576b20fc1a389
Holden Omans <holden.omans@gmail.com>
2023-05-26 14:02:03 -0400

    fixed catalog namespace

--- src/scryfall/models/catalog.cr
+++ src/scryfall/models/catalog.cr
@@ -1,10 +1,12 @@
 require "json"
 
-class Catalog
-  include JSON::Serializable
+module Scryfall
+  class Catalog
+    include JSON::Serializable
 
-  property object : String
-  property uri : String
-  property total_values : Int32
-  property data : Array(String)
+    property object : String
+    property uri : String
+    property total_values : Int32
+    property data : Array(String)
+  end
 end
